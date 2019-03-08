f96bc011929f4070e33daf030fd37a692d9cfdfd
Ahmet <ahmetcilingir@outlook.com>
2019-03-08 17:20:48 +0300

    İletişim Silindi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { TalepFormuComponent } from './talep-formu/talep-formu.component';
 import { ZararlilarComponent } from './zararlilar/zararlilar.component';
 import { IlaclamaYontemleriComponent } from './ilaclama-yontemleri/ilaclama-yontemleri.component';
 import { GaleriComponent } from './galeri/galeri.component';
-import { IletisimComponent } from './iletisim/iletisim.component';
 import { HizmetlerimizComponent } from './hizmetlerimiz/hizmetlerimiz.component';
 import { ReferanslarComponent } from './referanslar/referanslar.component';
 import { AnasayfaComponent } from './anasayfa/anasayfa.component';
@@ -22,7 +21,6 @@ const appRoutes: Routes = [
   { path: 'referanslar', component: ReferanslarComponent },
   { path: 'ilaclama-yontemleri', component: IlaclamaYontemleriComponent },
   { path: 'galeri', component: GaleriComponent },
-  { path: 'iletisim', component: IletisimComponent },
   { path: 'hakkimizda', component: HakkimizdaComponent },
   {
     path: '', redirectTo: "anasayfa", pathMatch: 'full'
@@ -36,7 +34,6 @@ const appRoutes: Routes = [
     ZararlilarComponent,
     IlaclamaYontemleriComponent,
     GaleriComponent,
-    IletisimComponent,
     HizmetlerimizComponent,
     ReferanslarComponent,
     AnasayfaComponent,
